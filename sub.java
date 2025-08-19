30b0c8c2303d0dacda95b65e8eeae93427a9559e
amoolya581 <damoolya_cse230581@mgit.ac.in>
2025-08-19 14:12:03 +0530

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,28 @@
+// This is a simple Java program
+
+public class Main {
+    // Function to print "Hello, World!"
+    public static void helloWorld() {
+        System.out.println("Hello, World!");
+    }
+
+    // Function to subtract two numbers
+    public static int subtractNumbers(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        // Calling the helloWorld method
+        helloWorld();
+
+        // Defining two numbers
+        int num1 = 15;
+        int num2 = 7;
+
+        // Calling the subtractNumbers method
+        int result = subtractNumbers(num1, num2);
+
+        // Printing the result
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + result);
+    }
+}
