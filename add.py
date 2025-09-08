14efa3c229855f0c604835aed1dc40a7b93a73f5
amoolya581 <damoolya_cse230581@mgit.ac.in>
2025-09-08 19:48:35 +0530

    Update add.py

--- add.py
+++ add.py
@@ -2,7 +2,7 @@
 
 # Function to print "Hello, World!"
 def hello_world():
-    print("Hello, World!")
+    print("Hello, Addition!")
 
 # Function to add two numbers
 def add_numbers(a, b):
@@ -11,7 +11,7 @@ def add_numbers(a, b):
 # Calling the functions
 hello_world()
 
-num1 = 5
+num1 = 15
 num2 = 10
 result = add_numbers(num1, num2)
 
